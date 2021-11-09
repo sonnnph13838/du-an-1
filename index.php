a3b509760d85bb3888a42fa72dc41c40683e12f2
Thuanncoi2002 <Thuanncoi2002@users.noreply.github.com>
2021-11-09 14:56:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php
-echo "Chào";
-echo "Thuận";
-echo "Dương";
-echo "thái";
-echo "tuấn";
-echo "Hôm nay ngày 9/11/2021 Thuận đã và github để xem có code gì mới không :)) ";
+
+echo"Đây là task Demo của Thuận";
