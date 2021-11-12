9968a37ed04b7b3fada58b2b585d234cf954fe29
tuandtph13846 <93896260+tuandtph13846@users.noreply.github.com>
2021-11-12 21:33:02 +0700

    task3-sửa-1

--- admin/taikhoan/edit_taikhoan.php
+++ admin/taikhoan/edit_taikhoan.php
@@ -4,66 +4,33 @@ include '../../model/pdo.php';
 $nguoi_dung = ds_nguoi_dung();
 ?>
 <form action="index.php?act=edit_taikhoan.php" method="POST" enctype="multipart/form-data">
-    <div class="row">
-        <div class= "col">
-            <div class="form-group">
-                <label for="">id</label>
-                <input class="form-control" type="hidden" name="customer"  value="<?= $nguoi_dung['id']?>">
-            </div>
-        </div>
-        <div class="col">
-            <div class="form-group">
-                <label for="">Tài khoản</label>
-                <input class="form-control" type="text" name="tai_khoan" value="<?= $nguoi_dung['tai_khoan']?>">
-            </div>
-        </div>
-        <div class="col">
-            <div class="form-group">
-                <label for="">Mật khẩu</label>
-                <input class="form-control" type="number" name="mat_khau" value="<?= $nguoi_dung['mat_khau']?>">
-            </div>
-        </div>
-        <div class="col">
-            <div class="form-group">
-                <label for="">email</label>
-                <input class="form-control" type="text" name="email" value="<?= $nguoi_dung['email']?>">
-            </div>
-        </div>
-        <div class="col">
-            <div class="form-group">
-                <label for="">dia_chi</label>
-                <input class="form-control" type="text" name="ho_ten"  value="<?= $nguoi_dung['dia_chi']?>">
-            </div>
-        </div>
-        <div class="col">
-            <div class="form-group">
-                <label for="">sdt</label>
-                <input class="form-control" type="number" name="sdt" value="<?= $nguoi_dung['sdt']?>">
-            </div>
-        </div>
-        <div class="col">
-            <div class="form-group">
-            <label for="">Vai trò</label>
-                <select name="vai_tro" >
-                    <option value="0">user</option>
-                    <option value="1">admin</option>
-                </select>
-            </div>
-        </div>
-
-        <div class="col">
-            <div class="form-group">
-                <label for="">họ tên</label>
-                <input class="form-control" type="text" name="ho_ten" value="<?= $nguoi_dung['ho_ten']?>">
-            </div>
-        </div>
- 
+    <div>
+        id : <input type="hidden"name="id" value="<?php echo($nguoi_dung['id'])?>">
+    </div>
+    <div>
+        Họ Tên: <input type="text" name="name" value="<?php echo($nguoi_dung['ho_ten'])?>">
+    </div>
+    <br>
+    <div>
+        Tài khoản: <input type="text" name="taikhoan" value="<?php echo($nguoi_dung['tai_khoan'])?>">
+    </div>
+    <br>
+    <div>
+        Mật Khẩu: <input type="password" name="matkhau" value="<?php echo($nguoi_dung['mat_khau'])?>">
+    </div>
+    <br>
+    <div>
+        Email: <input type="text" name="email" value="<?php echo($nguoi_dung['email'])?>">
+    </div>
+    <br>
+    <div>
+        Địa chỉ: <input type="text" name="diachi" value="<?php echo($nguoi_dung['dia_chi'])?>">
+    </div>
+    <br>
+    <div>
+        Số điện thoại: <input type="text" name="sdt" value="<?php echo($nguoi_dung['sdt '])?>">
     </div>
     <button class="btn" type="submit" name="btn_update_kh">Thêm</button>
     <button class="btn"><a href="">Quay lại</a></button>
 
 </form>
-
-
-
-
--- model/nguoi-dung.php
+++ model/nguoi-dung.php
@@ -6,7 +6,7 @@ function nguoi_dung_update($id,$tai_khoan,$mat_khau,$email,$dia_chi,$sdt,$vai_tr
     pdo_execute($sql,$id,$tai_khoan,$mat_khau,$email,$dia_chi,$sdt,$vai_tro,$ho_ten);
 }
 function ds_nguoi_dung(){
-    $sql = "SELECT * from nguoi_dung where id=id ";
+    $sql = "SELECT * from nguoi_dung where id=$id ";
     $ds_nguoi_dung =  pdo_query($sql);
     return $ds_nguoi_dung;
 }
