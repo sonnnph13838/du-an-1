bbaada59befbb4af2504f69c57c05084aab06514
sonnnph13838 <sonnnph13838@fpt.edu.vn>
2021-11-12 22:08:16 +0700

    Update xoa-nguoi-dung.php

--- admin/nguoi-dung/xoa-nguoi-dung.php
+++ admin/nguoi-dung/xoa-nguoi-dung.php
@@ -4,5 +4,5 @@ include '../../model/nguoi-dung.php';
 if(isset($_GET['id'])&& ($_GET['id']>0)){
     xoa_nguoi_dung($_GET['id']);
 }
-header('location:ds-nguoi-dung.php');
+header('location: ds-nguoi-dung.php');
 ?>
