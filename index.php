e626c31b24ce57cdf6c9ec8bb5b2481648edc19d
Thuanncoi2002 <Thuanncoi2002@users.noreply.github.com>
2021-11-12 21:17:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,12 @@ session_start();
 $url = isset($_GET['url']) ? $_GET['url'] : "/";
 include "./model/pdo.php";
 include "./model/nguoi-dung.php";
+include "view/dang-ki.php";
 include "view/post-dang-ki.php";
 switch ($url) {
+	case '/':
+		echo "đây là trang chủ";
+		break;
 	case 'dang-ki':
 		require_once "./view/dang-ki.php";
 		break;
