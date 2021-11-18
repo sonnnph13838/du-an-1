388eabda4e89cb9b757c5386fcacdf81709e1a20
Thuanncoi2002 <Thuanncoi2002@users.noreply.github.com>
2021-11-18 16:32:27 +0700

    upadate

--- client/business/dangky.php
+++ client/business/dangky.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once '././dao/user.php';
 function formdk()
 {
 	clien_dk('layout/form/formdk.php');
@@ -9,14 +9,13 @@ function postdk()
 {
 	if (isset($_POST['dangki']) && ($_POST['dangki'])) {
 		$hoten = $_POST['name'];
-		// $taikhoan = $_POST['taikhoan'];
-		// $matkhau = $_POST['matkhau'];
-		// $email = $_POST['email'];
-		// $diachi = $_POST['diachi'];
-		// $sdt = $_POST['sdt'];
-		// insert_nguoidung($taikhoan, $matkhau, $email, $diachi, $sdt);
-		// $thongbao = "Đăng kí thành công. Vui lòng đăng nhập để  thực hiện các chức năng!!";
-		// echo "$thongbao";
-		dd($hoten);
+		$taikhoan = $_POST['taikhoan'];
+		$matkhau = $_POST['matkhau'];
+		$email = $_POST['email'];
+		$diachi = $_POST['diachi'];
+		$sdt = $_POST['sdt'];
+		insert_nguoidung($taikhoan, $matkhau, $email, $diachi, $sdt);
+		$thongbao = "Đăng kí thành công. Vui lòng đăng nhập để  thực hiện các chức năng!!";
+		echo "$thongbao";
 	}
 }
