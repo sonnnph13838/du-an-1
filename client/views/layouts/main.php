25ccd9de3daf2290d08244ef7fd9efe40d871a41
duongdtph13852 <79820347+duongdtph13852@users.noreply.github.com>
2021-11-21 21:11:20 +0700

    update-login-moi

--- client/views/layouts/main.php
+++ client/views/layouts/main.php
@@ -49,7 +49,7 @@
           <a href="<?= CLIENT_ASSET ?>dist/#order"><i class="fas fa-shopping-cart"></i></a>
         <?php }else{ ?>
         <nav class="navbar">
-        <a href="<?= BASE_URL . 'dang-xuat'?>">Đăng Nhập</a>
+        <a href="<?= BASE_URL . 'dang-nhap'?>">Đăng Nhập</a>
         <a href="<?= BASE_URL . 'dang-ki'?>" style="margin-left: 5px; border-left: 1px solid #666; padding-left: 5px;">Đăng kí</a>
         <a href="<?= CLIENT_ASSET ?>dist/#order"><i class="fas fa-shopping-cart"></i></a>
       </nav>
