c62a3fe70404366775fa27b1bf390b7a2a57d8a3
duongdtph13852 <79820347+duongdtph13852@users.noreply.github.com>
2021-11-29 00:02:13 +0700

    task-11-update-css

--- client/views/ctsp/sp.php
+++ client/views/ctsp/sp.php
@@ -1,29 +1,112 @@
-<?php foreach ($sp as $gg):?>
-  
-    <div style="margin-top: 100px; margin-left:50px">
-        <div class="container" style="display: grid; grid-template-columns: 1fr 1fr">
-            <div class="detail-restaurant-img">
-            <img src="<?= CLIENT_ASSET ?>dist/images/g-1.jpg" alt="" />
-            </div>
-            <div class="content">
-            <h1>Tên Sản Phẩm:<br>
-              <?=$gg['name_food']?></h1>
-            <p style="margin-top: 50px; font-size:20px">Giá:<br>
-            <?=$gg['price_food']?></p>
-            <p style="margin-top: 50px; font-size:15px">Chi Tiết:<br>
-            <?=$gg['detail_food']?></p>
-            <?php endforeach ?>
-            <?php foreach ($sps as $option):?>
-            <button style=
-            "margin-top: 50px;
-            background-color: #666;
-            border-radius: 5px;
-            width: 100px"><?=$option['name_option']?></button>
-            <p><?=$option['price_option']?></p>
-            <?php endforeach ?>
-            <a href="#" class="btn">ordern now</a>
+<!--Section: Block Content-->
+<div class="mgt-dn lefa mg-fter">
+<section class="mb-5">
+
+  <div class="row">
+    <div class="col-md-6 mb-4 mb-md-0">
+      <div id="mdb-lightbox-ui"></div>
+
+      <div class="mdb-lightbox">
+
+        <div class="row product-gallery mx-1">
+
+          <div class="col-12 mb-0">
+            <figure class="view overlay rounded z-depth-1 main-img">
+              <a href="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15a.jpg"
+                data-size="710x823">
+                <img src="<?= CLIENT_ASSET ?>dist/images/g-1.jpg" alt="" />
+              </a>
+            </figure>
+            <figure class="view overlay rounded z-depth-1" style="visibility: hidden;">
+              <a href="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12a.jpg"
+                data-size="710x823">
+                
+                  
+              </a>
+            </figure>
+            <figure class="view overlay rounded z-depth-1" style="visibility: hidden;">
+              <a href="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg"
+                data-size="710x823">
+                
+                  
+              </a>
+            </figure>
+            <figure class="view overlay rounded z-depth-1" style="visibility: hidden;">
+              <a href="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14a.jpg"
+                data-size="710x823">
+               
+                  
+              </a>
+            </figure>
           </div>
+         
         </div>
-        </div>
-        
-    <!-- footer section  -->
+
+      </div>
+
+    </div>
+    <div class="col-md-6">
+    <?php foreach ($sp as $gg):?>
+      <h5> <?=$gg['name_food']?></h5>
+      <p class="mb-2 text-muted text-uppercase small">Shirts</p>
+      
+      <p><span class="mr-1"><strong><?=$gg['price_food']?></strong></span></p>
+      <p class="pt-1"><?=$gg['detail_food']?></p>
+        <?php endforeach ?>
+      <div class="table-responsive">
+        <table class="table table-sm table-borderless mb-0">
+         
+        </table>
+      </div>
+      <hr>
+      <div class="table-responsive mb-2">
+        <table class="table table-sm table-borderless">
+          <tbody>
+            <tr>
+              <td class="pl-0 pb-0 w-25">Quantity</td>
+              <td class="pb-0">Select size</td>
+            </tr>
+            <tr>
+              <td class="pl-0">
+                <div class="def-number-input number-input safari_only mb-0">
+                  <button onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
+                    class="minus"></button>
+                  <input class="quantity" min="0" name="quantity" value="1" type="number">
+                  <button onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
+                    class="plus"></button>
+                </div>
+              </td>
+              <td>
+                <div class="mt-1">
+                  <div class="form-check form-check-inline pl-0">
+                    <input type="radio" class="form-check-input" id="small" name="materialExampleRadios"
+                      checked>
+                    <label class="form-check-label small text-uppercase card-link-secondary"
+                      for="small">Small</label>
+                  </div>
+                  <div class="form-check form-check-inline pl-0">
+                    <input type="radio" class="form-check-input" id="medium" name="materialExampleRadios">
+                    <label class="form-check-label small text-uppercase card-link-secondary"
+                      for="medium">Medium</label>
+                  </div>
+                  <div class="form-check form-check-inline pl-0">
+                    <input type="radio" class="form-check-input" id="large" name="materialExampleRadios">
+                    <label class="form-check-label small text-uppercase card-link-secondary"
+                      for="large">Large</label>
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <button type="button" class="btn btn-primary btn-md mr-1 mb-2">Buy now</button>
+      <button type="button" class="btn btn-light btn-md mr-1 mb-2"><i
+          class="fas fa-shopping-cart pr-2"></i>Add to cart</button>
+    </div>
+  </div>
+   
+</section>
+</div>
+
+<!--Section: Block Content-->
--- client/views/layouts/main.php
+++ client/views/layouts/main.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>complete responsive food website design tutorial</title>
+<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>complete responsive food website design tutorial</title>
+	<!-- bootrap-link -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<!-- font awesome cdn link  -->
 
-    <!-- font awesome cdn link  -->
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-    />
 
-    <!-- custom css file link  -->
-    <link rel="stylesheet" href="<?= CLIENT_ASSET ?>dist/css/style.css" />
+	<!-- custom css file link  -->
+	<link rel="stylesheet" href="<?= CLIENT_ASSET ?>dist/css/style.css" />
   </head>
   <body>
     <!-- header section starts  -->
