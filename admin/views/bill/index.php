8af69346db34a40520c513d40c5f903a11e92e07
sonnnph13838 <sonnnph13838@fpt.edu.vn>
2021-12-05 22:37:38 +0700

    update

--- admin/views/bill/index.php
+++ admin/views/bill/index.php
@@ -1,26 +1,19 @@
-<!-- <style>
-    table {
-        width: 100%;
+<style>
+    .button_sua {
+        border: none;
+        background: white;
+        color: blue;
     }
-
-    td {
-        text-align: center;
-    }
-
-    th {
-        text-align: center;
-        color: red;
-    }
-</style> -->
+</style>
 <div class="row">
     <div class="col-12">
         <div class="card">
-        <div class="card-header">
+            <div class="card-header">
                 <form action="" method="get">
                     <div class="row">
                         <div class="col-6">
                             <div class="form-group">
-                                <input type="text" name="keyword" value="<?=$keyword ?>" class="form-control" placeholder="Tìm kiếm..." aria-describedby="helpId">
+                                <input type="text" name="keyword" value="<?= $keyword ?>" class="form-control" placeholder="Tìm kiếm..." aria-describedby="helpId">
                             </div>
                         </div>
                     </div>
@@ -40,48 +33,62 @@
                     </thead>
                     <tbody>
                         <?php foreach ($cates as $item) : ?>
-                        <tr>
-                            <td><?= $item['id_bill'] ?></td>
-                            <td><?= $item['name_bill'] ?></td>
-                            <td><?= $item['address'] ?></td>
-                            <td><?= $item['tel'] ?></td>
-                            <?php 
-                            $payment = $item['payment'] ;
-                            if($item['payment'] == 0){
-                                $payment = "Thanh toán khi nhận hàng";
-                            }elseif($item['payment'] == 1){
-                                $payment = "Thanh toán bằng thẻ tín dụng";
-                            }elseif($item['payment'] == 2){
-                                $payment = "Thanh toán bằng zaloPay";
-                            }
-                            ?>
-                            <td><?= $payment ?></td>
-                            <td><?= $item['total'] ?></td>
-                            <td><?= $item['date'] ?></td>
-                            <?php 
-                            $status = $item['status'] ;
-                            if($item['status'] == 0){
-                                $status = "Chờ xử lý";
-                            }elseif($item['status'] == 1){
-                                $status = "Đang giao hàng";
-                            }elseif($item['status'] == 2){
-                                $status = "Đã lấy hàng";
-                            }elseif($item['status'] == 3){
-                                $status = "Trả lại hàng";
-                            }
-                            ?>
-                            <td><?=  $status ?> <a href="<?= ADMIN_URL . 'bill/edit-status?id='. $item['id_bill'] ?>"><i class="fas fa-edit"></i></a></td>
-                           
-                            <td>
-                                <a href="javascript:;"
-                                    onclick="confirm_remove('<?= ADMIN_URL . 'bill/del-bill?id='. $item['id_bill'] ?>', '<?= $item['name_bill']?>')"
-                                    class="btn btn-sm btn-danger">
-                                    <i class="fas fa-trash"></i>
-                                </a>
-                            </td>
+                            <tr>
+                                <td><?= $item['id_bill'] ?></td>
+                                <td><?= $item['name_bill'] ?></td>
+                                <td><?= $item['address'] ?></td>
+                                <td><?= $item['tel'] ?></td>
+                                <?php
+                                $payment = $item['payment'];
+                                if ($item['payment'] == 0) {
+                                    $payment = "Thanh toán khi nhận hàng";
+                                } elseif ($item['payment'] == 1) {
+                                    $payment = "Thanh toán bằng thẻ tín dụng";
+                                } elseif ($item['payment'] == 2) {
+                                    $payment = "Thanh toán bằng zaloPay";
+                                }
+                                ?>
+                                <td><?= $payment ?></td>
+                                <td><?= $item['total'] ?> VNĐ</td>
+                                <td><?= $item['date'] ?></td>
+                                <?php
+                                $status = $item['status'];
+                                if ($item['status'] == 0) {
+                                    $status = "Chờ xử lý";
+                                } elseif ($item['status'] == 1) {
+                                    $status = "Đang giao hàng";
+                                } elseif ($item['status'] == 2) {
+                                    $status = "Đã lấy hàng";
+                                } elseif ($item['status'] == 3) {
+                                    $status = "Trả lại hàng";
+                                }
+                                ?>
+
+                                <td>
+
+                                    <form action="<?= ADMIN_URL . 'bill/edit-status' ?>" method="post">
+                                        <input type="hidden" name="id" value="<?= $item['id_bill'] ?>">
+                                        <select name="status" id="" style="border : none" ;>
+                                            <option><?= $status ?></option>
+                                            <option value="0">Chờ xử lý</option>
+                                            <option value="1">Đang giao hàng</option>
+                                            <option value="2">Đã lấy hàng</option>
+                                            <option value="3">Trả lại hàng</option>
+                                        </select>
+                                        <input type="submit" class="button_sua" value="Sửa">
+                                        <!-- <i class="fas fa-edit"></i> -->
+
+                                    </form>
+                                </td>
+
+                                <td>
+                                    <a href="javascript:;" onclick="confirm_remove('<?= ADMIN_URL . 'bill/del-bill?id=' . $item['id_bill'] ?>', '<?= $item['name_bill'] ?>')" class="btn btn-sm btn-danger">
+                                        <i class="fas fa-trash"></i>
+                                    </a>
+                                </td>
 
 
-                        </tr>
+                            </tr>
                         <?php endforeach ?>
                     </tbody>
                 </table>
