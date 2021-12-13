716c487dbf70664b41b630f7b1141f97c77e8e5b
Thuanncoi2002 <Thuanncoi2002@users.noreply.github.com>
2021-12-13 21:22:55 +0700

    update

--- client/views/tai-khoan/dang-ki.php
+++ client/views/tai-khoan/dang-ki.php
@@ -25,6 +25,9 @@
 	<input type="submit" value="Đăng kí" name="dangki">
 </form> -->
 <!-- From có css -->
+<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js"></script>
+<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.min.js" type="text/javascript"></script>
+<script src="demoValidation.js" type="text/javascript"></script>
 <div class="mgt-dn cnter mg-fterr">
 	<div class="wrapper">
 		<?php if (isset($_GET['msg']) && ($_GET['msg'] != "")) : ?>
@@ -34,7 +37,7 @@
 		<?php endif ?>
 		<div class="forgot-pass">
 			<h3>Đăng kí</h3>
-			<form action="postdk" method="post">
+			<form action="postdk" method="post" id="demoForm">
 				<div class="field email">
 					<div class="input-area">
 						<input type="email" placeholder="Email" name="email">
@@ -43,13 +46,13 @@
 				</div>
 				<div class="field password">
 					<div class="input-area">
-						<input type="password" placeholder="Mật khẩu" name="matkhau">
+						<input type="password" id="matkhau" placeholder="Mật khẩu" name="matkhau">
 						<i class="icon fas fa-lock"></i>
 					</div>
 				</div>
 				<div class="field password">
 					<div class="input-area">
-						<input type="password" placeholder="Nhập Lại Mật khẩu" name="matkhau2">
+						<input type="password" id="matkhau2" placeholder="Nhập Lại Mật khẩu" name="matkhau2">
 						<i class="icon fas fa-lock"></i>
 					</div>
 				</div>
@@ -72,3 +75,34 @@
 		</div>
 	</div>
 </div>
+<script>
+$().ready(function() {
+	$("#demoForm").validate({
+		onfocusout: false,
+		onkeyup: false,
+		onclick: false,
+		rules: {
+			"email": {
+				required: true,
+				maxlength: 15
+			},
+			"matkhau": {
+				required: true,
+				minlength: 8
+			},
+			"matkhau2": {
+				equalTo: "#matkhau",
+				minlength: 8
+			},
+			"diachi": {
+				required: true,
+				maxlength: 15
+			},
+			"sdt": {
+				required: true,
+				maxlength: 15
+			},
+		}
+	});
+});
+</script>
