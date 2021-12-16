a37648ad7f6d1215952edfe83e86c0de7078ddfd
duongdtph13852 <79820347+duongdtph13852@users.noreply.github.com>
2021-12-16 16:57:29 +0700

    Update dang-nhap.php

--- client/views/tai-khoan/dang-nhap.php
+++ client/views/tai-khoan/dang-nhap.php
@@ -1,3 +1,12 @@
+
+	    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js"></script>
+	    <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.min.js" type="text/javascript"></script>
+	    <script src="demoValidation.js" type="text/javascript"></script>
+	    <style>
+		label.error{
+			color: red;
+		}
+	    </style>
 <div class="mgt-dn cnter mg-fterr">
 <div class="wrapper form-dn">
     <h3>Đăng nhập</h3>
@@ -5,7 +14,7 @@
 		if (isset($_GET['msg']) && ($_GET['msg']) != "")
 			echo "<h3>" . $_GET['msg'] . "</h3>"
 		?>
-    <form action="post-login" method="POST">
+    <form action="post-login" id="demoForm" method="POST">
       <div class="field email">
         <div class="input-area">
           <input type="email" name="email" placeholder="Email Address">
@@ -32,3 +41,22 @@
     <div class="sign-txt">Bạn chưa có tài khoản? <a href="client-dangki">Đăng kí ngay</a></div>
 </div>
 </div>
+<script>
+$().ready(function() {
+	$("#demoForm").validate({
+		onfocusout: false,
+		onkeyup: false,
+		onclick: false,
+		rules: {
+			"email": {
+				required: true,
+				maxlength: 15
+			},
+			"mat_khau": {
+				required: true,
+				minlength: 8
+			},
+		}
+	});
+});
+  </script>
