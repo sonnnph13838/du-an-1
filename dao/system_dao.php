1eadb5e8c687d69505480f0eab9aaa604b6dc91e
tuandtph13846 <93896260+tuandtph13846@users.noreply.github.com>
2021-12-17 09:36:13 +0700

    top

--- dao/system_dao.php
+++ dao/system_dao.php
@@ -1,6 +1,6 @@
 <?php
 function get_connect(){
-    $connect = new PDO("mysql:host=127.0.0.1;dbname=nhom_6;charset=utf8", "root", "");
+    $connect = new PDO("mysql:host=127.0.0.1;dbname=nhom_six;charset=utf8", "root", "");
     return $connect;
 }
 
